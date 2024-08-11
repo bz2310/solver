68a4a5732880c726dcc1c3b75131ab97eb54fb92
MSPsolve <maxschindlerpoker@gmail.com>
2024-08-11 12:25:51 -0400

    .

--- src/card.rs
+++ src/card.rs
@@ -60,9 +60,38 @@ impl Default for CardConfig {
     }
 }
 
+#[derive(Debug, Clone)]
+#[cfg_attr(feature = "bincode", derive(Decode, Encode))]
+pub struct StudCardConfig {
+    /// Initial range of each player.
+    pub range: [Range; 2],
+
+    pub third_street: (Card, Card),
+    pub fourth_street: (Card, Card),
+    pub fifth_street: (Card, Card),
+    pub sixth_street: (Card, Card),
+}
+
+impl Default for StudCardConfig {
+    #[inline]
+    fn default() -> Self {
+        Self {
+            range: Default::default(),
+            third_street: (NOT_DEALT, NOT_DEALT),
+            fourth_street: (NOT_DEALT, NOT_DEALT),
+            fifth_street: (NOT_DEALT, NOT_DEALT),
+            sixth_street: (NOT_DEALT, NOT_DEALT),
+        }
+    }
+}
+
 type PrivateCards = [Vec<(Card, Card)>; 2];
 
+type StudPrivateCards = [Vec<(Card, Card, Card)>; 2];
+
 type Indices = [Vec<u16>; 2];
+
+type StudIndices = [Vec<u32>; 2];
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub(crate) struct StrengthItem {
@@ -447,5 +476,78 @@ mod tests {
                 k += 1;
             }
         }
+    }
+}
+
+impl StudCardConfig {
+    pub(crate) fn valid_indices(
+        &self,
+        private_cards: &StudPrivateCards,
+    ) -> (StudIndices, Vec<StudIndices>, Vec<StudIndices>, Vec<StudIndices>) {
+
+        // /TODO NEXT
+
+    pub(crate) fn hand_strength(
+        &self,
+        private_cards: &PrivateCards,
+    ) -> Vec<[Vec<StrengthItem>; 2]> {
+        let mut ret = vec![Default::default(); 52 * 51 / 2];
+
+        let mut board = Hand::new();
+        for &card in &self.flop {
+            board = board.add_card(card as usize);
+        }
+
+        for board1 in 0..52 {
+            for board2 in board1 + 1..52 {
+                if !board.contains(board1 as usize)
+                    && !board.contains(board2 as usize)
+                    && (self.turn == NOT_DEALT || board1 == self.turn || board2 == self.turn)
+                    && (self.river == NOT_DEALT || board1 == self.river || board2 == self.river)
+                {
+                    let board = board.add_card(board1 as usize).add_card(board2 as usize);
+                    let mut strength = [
+                        Vec::with_capacity(private_cards[0].len() + 2),
+                        Vec::with_capacity(private_cards[1].len() + 2),
+                    ];
+
+                    for player in 0..2 {
+                        // add the weakest and strongest sentinels
+                        strength[player].push(StrengthItem {
+                            strength: 0,
+                            index: 0,
+                        });
+                        strength[player].push(StrengthItem {
+                            strength: u16::MAX,
+                            index: u16::MAX,
+                        });
+
+                        strength[player].extend(
+                            private_cards[player].iter().enumerate().filter_map(
+                                |(index, &(c1, c2))| {
+                                    let (c1, c2) = (c1 as usize, c2 as usize);
+                                    if board.contains(c1) || board.contains(c2) {
+                                        None
+                                    } else {
+                                        let hand = board.add_card(c1).add_card(c2);
+                                        Some(StrengthItem {
+                                            strength: hand.evaluate() + 1, // +1 to avoid 0
+                                            index: index as u16,
+                                        })
+                                    }
+                                },
+                            ),
+                        );
+
+                        strength[player].shrink_to_fit();
+                        strength[player].sort_unstable();
+                    }
+
+                    ret[card_pair_to_index(board1, board2)] = strength;
+                }
+            }
+        }
+
+        ret
     }
 }
